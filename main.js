6d4c4032a738927750b72b6d89836fa28560725e
JaguarDrinks <facundorossetto@gmail.com>
2023-11-09 16:12:10 +0000

    Show score Show current score for both player and cpu. The idea is to make a 5 round game, so I made a counter with game() function, and next Im going to create a restart option

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let play = document.getElementById("play")
 let cpu = document.getElementById("cpu")
 let player = document.getElementById("player")
 let result = document.getElementById("result")
+let playerScore = document.getElementById("player-score")
+let cpuScore = document.getElementById("cpu-score")
+
 
 let computerSelection = ""
 function getComputerChoice(){
@@ -32,32 +35,58 @@ function getPlayerChoice(){
 }
 
 let winner = ""
+let playerCounter = 0
+let cpuCounter = 0
 function roundPlay(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         winner = "Its a tie!"
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         winner = "Paper beats rock, you lost!"
+        cpuCounter ++
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         winner = "Rock beats scissors, you won!"
+        playerCounter ++
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         winner = "Paper beats rock, you won!"
+        playerCounter ++
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         winner = "Scissors beats paper, you lost!"
+        cpuCounter ++
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         winner = "Rock beats scissors, you lost!"
+        cpuCounter ++
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         winner = "Scissors beats paper, you won!"
+        playerCounter ++
     } else {
         winner = "Oops! Try again with a valid option"
     }
-    
 }
 
+
+function showRestart() {
+
+}
+
+let gamesPlayed = 0
+function game() {
+    gamesPlayed++
+    if (gamesPlayed < 5) {
+        roundPlay(playerSelection, computerSelection)
+    } else if (gamesPlayed = 5) {
+        roundPlay(playerSelection, computerSelection)
+        console.log("GAME OVER")
+        showRestart()
+    }
+    console.log("Games played: "+gamesPlayed)
+}
 
 play.addEventListener("click", ()=> {
     getComputerChoice()
     getPlayerChoice()
-    roundPlay(playerSelection, computerSelection)
+    game()
     result.innerHTML = winner
+    playerScore.innerHTML = playerCounter
+    cpuScore.innerHTML = cpuCounter
 })
 
